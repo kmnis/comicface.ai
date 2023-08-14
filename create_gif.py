e0bd4b7d30259b3e2a63ff76f7491f4788da8fb8
Manish Kumar <sihagmanish36@gmail.com>
2023-08-14 08:47:23 +0000

    Updated gif python file

--- create_gif.py
+++ create_gif.py
@@ -1,17 +1,22 @@
 from glob import glob
 from PIL import Image
 
-image_files = glob("saved_models/pix2pix/training_progress/*.png")
+model = "vae" # pix2pix
+image_files = glob(f"saved_models/{model}/training_progress/*.png")
 
 def numeric_sort_key(filename):
     return int(filename.split("/")[-1][:-4])
     
 image_files.sort(key=numeric_sort_key)
 
-print(len(image_files))
+print("Total images found:", len(image_files))
 
 images = [Image.open(f) for f in image_files]
-images = images[0:20] + images[25:30]
 
-durations = [150] * (len(images) - 1) + [1000]
-images[0].save("saved_models/pix2pix/training_progress/demo.gif", save_all=True, append_images=images[1:], duration=durations, loop=0)
+if model == "pix2pix":
+    images = images[0:20] + images[25:30]
+    durations = [150] * (len(images) - 1) + [1000]
+else:
+    durations = [50] * (len(images) - 1) + [1000]
+
+images[0].save(f"saved_models/{model}/training_progress/{model}_training.gif", save_all=True, append_images=images[1:], duration=durations, loop=0)
